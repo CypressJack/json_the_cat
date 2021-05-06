5735c3371f1637e34f5b2861b3445056a98ee220
archaemedes <wadecroft@gmail.com>
2021-05-06 19:06:33 +0000

    Tested out the cat api with the request library

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
-const request = require('request');
+const request = require("request");
+const args = process.argv.slice(2);
+const webAddress = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
+
+request(webAddress, (error, response, body) => {
+  if (error) {
+    console.log('There was an error with the request.', error);
+  } else {
+    const data = JSON.parse(body)[0];
+    if (typeof data === "undefined") {
+      console.log("Unable to find that breed.");
+    } else {
+      console.log("description: ", data.description);
+    }
+  }
+});
